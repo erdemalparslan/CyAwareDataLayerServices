768be65b1b282a3336926dc5ea261f14a91f7b17
Erdem Alparslan <erdem.alparslan@stu.bahcesehir.edu.tr>
2015-11-10 22:13:28 -0800

    new adds

---
+++ CyAwareWebApi/Models/Entities/ETwitterProfile.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace CyAwareWebApi.Models.Entities
+{
+    public class ETwitterProfile : EntityBase
+    {
+        public string idStr { get; set; }
+        public string screenName { get; set; }
+        public int dailyMaxTweets { get; set; }
+        public int dailyMaxCAPITALLETTERRatio { get; set; }
+        public int dailyMaxFollowerChangeRatio { get; set; }
+        public int dailyMaxFalloweeChangeRatio { get; set; }
+        public string searchStringForUnusualContent { get; set; }
+        public bool isHacked { get; set; }
+    }
+}
---
+++ CyAwareWebApi/Models/Results/RModule3.cs
@@ -0,0 +1,18 @@
+﻿/* 
+This module is about Instagram activity checker module
+*/
+
+namespace CyAwareWebApi.Models.Results
+{
+    public class RModule3 : ResultBase
+    {
+        public string idStr { get; set; }
+        public byte[] actualProfilePictureMD5 { get; set; }
+        public string actualScreenName { get; set; }
+        public int actualPosts { get; set; }
+        public int actualCAPITALLETTERRatio { get; set; }
+        public int actualFollowerChangeRatio { get; set; }
+        public int actualFalloweeChangeRatio { get; set; }
+        public string unusualContentFound { get; set; }
+    }
+}
